e7e67e1234b6c37c2e5893b7f11f1245a881c8f5
Lucas Pereira <Lucasgpereira@gmail.com>
2020-07-21 11:40:43 -0300

    Initial Commit

---
+++ iChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iChat
+//
+//  Created by Lucas Pereira on 21/07/20.
+//  Copyright © 2020 LucasChatOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
